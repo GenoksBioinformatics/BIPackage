611c878b7cd40d2906ba843e64ce22f8f56f519f
Zaf4 <zafermolbio@gmail.com>
2025-05-08 17:55:35 +0300

    re-format for cli file:fastq_read_counter.py

--- bipackage/src/fastqtools/fastq_read_counter.py
+++ bipackage/src/fastqtools/fastq_read_counter.py
@@ -4,6 +4,8 @@ from concurrent.futures import ThreadPoolExecutor
 
 import pandas as pd
 
+from bipackage.util.utilities import timer
+
 
 def count_reads_and_bases(fastq_file):
     try:
@@ -20,33 +22,80 @@ def count_reads_and_bases(fastq_file):
         return fastq_file, 0, 0
 
 
-directory = "/home/genwork2/03.Fastq_Output/R208genklimerged"
+def _test_fastq_read_counter(
+    directory="/home/genwork2/03.Fastq_Output/R208genklimerged",
+    output_path="/home/genwork2/03.Fastq_Output/R208genklimerged/R208genklimerged-stats.csv",
+):
+    files_to_process = [
+        os.path.join(directory, filename)
+        for filename in os.listdir(directory)
+        if filename.endswith(".fastq.gz") and not filename.startswith("Undetermined")
+    ]
 
-files_to_process = [
-    os.path.join(directory, filename)
-    for filename in os.listdir(directory)
-    if filename.endswith(".fastq.gz") and not filename.startswith("Undetermined")
-]
+    results = []
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(count_reads_and_bases, file): file for file in files_to_process}
 
-results = []
-with ThreadPoolExecutor() as executor:
-    futures = {executor.submit(count_reads_and_bases, file): file for file in files_to_process}
+        for future in futures:
+            filename, read_count, base_count = future.result()
+            results.append(
+                {
+                    "file": os.path.basename(filename),
+                    "reads": read_count,
+                    "bases": base_count,
+                }
+            )
 
-    for future in futures:
-        filename, read_count, base_count = future.result()
-        results.append(
-            {
-                "file": os.path.basename(filename),
-                "reads": read_count,
-                "bases": base_count,
-            }
-        )
+    df = pd.DataFrame(results)
 
-df = pd.DataFrame(results)
+    # print(df)
 
-print(df)
+    df.to_csv(
+        output_path,
+        index=False,
+    )
 
-df.to_csv(
-    "/home/genwork2/03.Fastq_Output/R208genklimerged/R208genklimerged-stats.csv",
-    index=False,
-)
+@timer
+def fastq_read_counter(directory: str, output_path: str) -> None:
+    """
+    Count fastq.
+
+    Parameters
+    ----------
+    directory : str
+        Path to directory.
+    output_path : str
+        Path to save csv file.
+
+    """
+    files_to_process = [
+        os.path.join(directory, filename)
+        for filename in os.listdir(directory)
+        if filename.endswith(".fastq.gz") and not filename.startswith("Undetermined")
+    ]
+
+    results = []
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(count_reads_and_bases, file): file for file in files_to_process}
+
+        for future in futures:
+            filename, read_count, base_count = future.result()
+            results.append(
+                {
+                    "file": os.path.basename(filename),
+                    "reads": read_count,
+                    "bases": base_count,
+                }
+            )
+
+    df = pd.DataFrame(results)
+
+    # print(df)
+
+    df.to_csv(output_path, index=False)
+
+    return
+
+
+if __name__ == "__main__":
+    _test_fastq_read_counter()
